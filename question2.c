f35bd8bdd86b9cea4829e49d86878fda15d40583
Shihir <yshihir@gmail.com>
2026-02-02 22:27:17 +0530

    Update question2.c

--- question2.c
+++ question2.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
-int main(){
-	int length,pos;
-	printf("enter the length of the array:");
-	scanf("%d",&length);
-	int arr[length];
-	for(int i = 0; i < length; i++){
-		scanf("%d",&arr[i]);
-	}
-	for(int i = 0; i < length; i++){
-		printf("%d ",arr[i]);
-	}
-	printf("enter the position at which element is to be deleted:");
-	scanf("%d",&pos);
-	for(int i = pos - 1; i < length - 1 ; i++){	
-			arr[i] = arr[i+1];
-	}
-	printf("Updated array: ");
-	    for (int i = 0; i <= length; i++) {
-	        printf("%d ", arr[i]);
-	    }
-	    printf("\n");
-	
-	return 0;
+
+int main() {
+    int length, pos;
+    printf("enter the length of the array:");
+    scanf("%d", &length);
+    int arr[length];
+    for (int i = 0; i < length; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < length; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("enter the position at which element is to be deleted:");
+    scanf("%d", &pos);
+    for (int i = pos - 1; i < length - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    printf("Updated array: ");
+    for (int i = 0; i < length - 1; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
 }

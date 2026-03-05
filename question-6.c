b0851e33c7c14e042917c8df1c79604e736a9bf4
Shihir <yshihir@gmail.com>
2026-03-05 17:10:15 +0530

    Add files via upload
    bruh

---
+++ question-6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+	int n;
+	printf("enter n:");
+	scanf("%d",&n);
+	int arr[n],unique_list[n];
+	for(int i = 0; i < n; i++){
+		printf("enter the %d element:",i + 1);
+		scanf("%d",&arr[i]);
+	}
+	int index = 0;
+	for(int i = 0; i < n; i++){
+		int count = 1;
+		for(int j = 0; j < n; j++){
+			if(arr[i] == unique_list[j]) count++;
+		}
+		if(count == 1){
+			unique_list[index] = arr[i];
+			index++;
+		}
+	}
+	for(int i = 0; i < index; i++){
+		printf("%d\n",unique_list[i]);
+	}
+}
